15d31b077c21fdbc8ad666e38251ceb3e9955cb8
pepesr-dev <pepessrr2@gmail.com>
2026-01-25 23:45:32 +0100

    pendiente de diseño de interfaz

---
+++ 01-taskList/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de tareas</title>
+</head>
+<body>
+    <header>
+        <h1>Lista de tareas</h1>
+        
+    </header>
+    <main>
+        
+    </main>
+    <footer>
+        <p>PepeSR 2026</p>
+    </footer>
+</body>
+</html>
